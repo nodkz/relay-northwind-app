b0fb9f049ef27cda6d3426de1a4960d3ec1dad61
nodkz <nodkz@mail.ru>
2016-08-29 11:43:42 +0600

    Add link to issue about `useRelay.default ? useRelay.default : useRelay`

--- app/clientRootComponent.js
+++ app/clientRootComponent.js
@@ -20,6 +20,7 @@ export default class ClientRoot extends Component { // eslint-disable-line
       <Router
         routes={routes}
         history={history}
+        // `.default` problem explained here https://github.com/relay-tools/react-router-relay/issues/192
         render={applyRouterMiddleware(useRelay.default ? useRelay.default : useRelay)} // eslint-disable-line
         environment={environment}
       />
